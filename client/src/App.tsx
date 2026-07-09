248cc73a1030c565d814e928c1c6eae3578059f8
arvind6206 <vishwkarmaarvind1222@gmail.com>
2026-07-09 12:36:13 +0530

    client

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from './components/ui/Button'
+
+function App() {
+  return (
+    <div>
+    <Button variant="primary" size="md" onClick={() => {}} text={"asd"}/>
+      
+    </div>
+  )
+}
+
+export default App
