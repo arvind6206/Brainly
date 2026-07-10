178adbd1bf661ca5a32f00f9be1efcfd2d0ef624
arvind6206 <vishwkarmaarvind1222@gmail.com>
2026-07-10 08:35:32 +0530

    backend done

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,69 +12,85 @@ dotenv.config();
 
 app.use(express.json());
 
-const PORT = process.env.PORT!
+const PORT = process.env.PORT!;
 
-app.use("/api/v1/user", userRouter) 
-app.use("/api/v1/user/create", contentRouter) 
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/user/create", contentRouter);
 
-app.post("/api/v1/brain/share", async(req, res) => {
-    const {share} = req.body;
+app.post("/api/v1/brain/share", async (req, res) => {
+  const { share } = req.body;
 
-    if(share){
-        await linkModel.create({
-            //@ts-ignore
-            userId: req.userId,
-            hash: random(10)
-        })
-    } else {
-        await linkModel.deleteOne({
-            //@ts-ignore
-            userId: req.userId,
+  if (share) {
+    const existingLink = await linkModel.findOne({
+        //@ts-ignore
+        userId: req.userId
+    })
+    if(existingLink){
+        return res.json({
+            hash: existingLink.hash
         })
     }
+    const hash = random(10);
+    await linkModel.create({
+      //@ts-ignore
+      userId: req.userId,
+      hash: hash,
+    });
     res.json({
-        msg: "Updated sharable link"
-    })
-
+      msg: "/share" + hash,
+    });
+  } else {
+    await linkModel.deleteOne({
+      //@ts-ignore
+      userId: req.userId,
+    });
+    res.json({
+      msg: "Removed link",
+    });
+  }
 });
 
-app.get("/api/v1/brain/:shareLink", authMiddleware, async(req, res) => {
-    const hash = req.params.shareLink
-    //@ts-ignore
-    const link = await linkModel.findOne({
-        hash
-    })
-    if(!link){
-        return res.status(411).json({
-            mag: "sorry incorrect input"
-        })
-    }
+app.get("/api/v1/brain/:shareLink", async (req, res) => {
+  const hash = req.params.shareLink;
+  //@ts-ignore
+  const link = await linkModel.findOne({
+    hash,
+  });
+  if (!link) {
+    return res.status(411).json({
+      msg: "sorry incorrect input",
+    });
+  }
 
-    const content = await contentModel.find({
-        userId: link.userId
-    })
+  const content = await contentModel.find({
+    userId: link.userId,
+  });
 
-    const user = await userModel.findOne({
-        userId: link.userId
-    })
+  const user = await userModel.findOne({
+    _Id: link.userId,
+  });
 
-    res.json({
-        username: user?.username,
-        content: content
-    })
+  if (!user) {
+    return res.status(411).json({
+      msg: "user not found, error should ideally not happen",
+    });
+  }
+
+  res.json({
+    username: user.username,
+    content: content,
+  });
 });
 
-async function main(){
-    if (!process.env.MONGO_URI) {
+async function main() {
+  if (!process.env.MONGO_URI) {
     throw new Error("MONGO_URI is not defined");
+  }
+  await mongoose.connect(process.env.MONGO_URI);
+  console.log("DB connected");
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
 }
-await mongoose.connect(process.env.MONGO_URI);
-console.log("DB connected")
-
-app.listen(PORT,()=>{
-    console.log(`Server is running on http://localhost:${PORT}`)
-});
-}
-main()
-
-
+main();
