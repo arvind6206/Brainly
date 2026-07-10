2f612e92aae62a61d8a8e91e8985b598ff935245
arvind6206 <vishwkarmaarvind1222@gmail.com>
2026-07-10 13:21:47 +0530

    sidebar

---
+++ client/src/components/Sidebar.tsx
@@ -0,0 +1,24 @@
+import { Logo } from "../icons/Logo";
+import { TwitterIcon } from "../icons/TwitterIcon";
+import { YoutubeIcon } from "../icons/YoutubeIcon";
+import { SidebarItem } from "./SidebarItem";
+
+export function Seidebar() {
+  return (
+    <div
+      className="h-screen bg-white border-r w-76 fixed
+        left-0 top-0 pl-6"
+    >
+      <div className="flex text-2xl pt-8 items-center">
+        <div className="pr-2 tex-purple-600">
+            <Logo/>
+        </div>
+        Brainly
+      </div>
+      <div className="pt-8 pl-4">
+        <SidebarItem text="Twitter" icon={<TwitterIcon />} />
+        <SidebarItem text="Youtube" icon={<YoutubeIcon />} />
+      </div>
+    </div>
+  );
+}
